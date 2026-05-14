c46d8ed50f15550905c62acda3d2ea33c254e981
sanika14-sp <gsanikapm14@gmail.com>
2026-05-14 18:07:16 +0530

    added two number

---
+++ Sum_of_number.js
@@ -0,0 +1,7 @@
+console.log("SUM OF TWO NUMBERS");
+c=4;
+d=5;
+let a= console.log("The first number is :",c);
+let b= console.log("The second number is:",d);
+let sum = c+d;
+console.log("The sum of the two numbers is: " + sum);
