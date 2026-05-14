55dcbee8725b5c0622863119324571551a286f5f
sanika14-sp <gsanikapm14@gmail.com>
2026-05-14 18:30:02 +0530

    numbers added using function

--- Sum_of_number.js
+++ Sum_of_number.js
@@ -5,3 +5,17 @@ let a= console.log("The first number is :",c);
 let b= console.log("The second number is:",d);
 let sum = c+d;
 console.log("The sum of the two numbers is: " + sum);
+
+
+
+// Sum of two number using function 
+
+function sumofnumber(a,b){
+    c=a+b;
+    return c;
+
+}
+let addition = sumofnumber(18,4);
+console.log("The sum of two numbers is: " + addition);
+
+
