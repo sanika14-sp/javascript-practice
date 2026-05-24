034a0e6730d98f50943463c17fd16a32c7c943bc
sanika14-sp <gsanikapm14@gmail.com>
2026-05-24 13:44:20 +0530

    used prompt,alert,console.error

---
+++ Browser_ques.js
@@ -0,0 +1,15 @@
+// Check whether the user can drive or not using prompt and alert 
+runAgain= true;
+while(runAgain){
+let age = parseInt(prompt("Enter your age:"));
+if(age<0){
+    console.error("Please enter a valid age");
+    break;
+}
+if(age>=18){
+    alert("you can drive");
+}else{
+    alert("you can't drive");
+}
+runAgain= confirm("DO you want to check again ?");
+}
