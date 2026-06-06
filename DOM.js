cd94709b253268624aa4ce38e8e59494078d282b
sanika14-sp <gsanikapm14@gmail.com>
2026-06-06 21:01:26 +0530

    brower events

--- DOM.js
+++ DOM.js
@@ -13,3 +13,22 @@ array = Array.from(document.getElementsByTagName("tr"));
 array.forEach((element)=>{
     element.style.backgroundColor="cyan";
 })
+
+
+document.addEventListener("click",function(e){
+    switch(e.target.id){
+        case "btn1":
+            alert("Wrong answer");
+            break;
+        case "btn2":
+            alert("Correct answer");
+            break;
+        case "btn3":
+            alert("Wrong answer");
+            break;
+        case "btn4":
+            alert("Wrong answer");
+            break;
+
+    }
+})
