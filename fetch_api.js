ec433b1542f907fc56cd871ec04c4cf80d3eb99c
sanika14-sp <gsanikapm14@gmail.com>
2026-06-16 23:59:53 +0530

    photo,location,no fetched

--- fetch_api.js
+++ fetch_api.js
@@ -1,13 +1,18 @@
-let getData=async()=>{
+let getUser= async()=>{
     try{
-     let user = document.getElementById("user").value;
-     let response = await fetch("https://randomuser.me/api/");
-     console.log("This is fetched");
-     let data= await response.json();
-     console.log(data);
-    }
+        let response= await fetch("https://randomuser.me/api/");
+        let data= await response.json();
+        let user= data.results[0];
+        console.log(data.results[0]);
+        document.getElementById("name").innerHTML=`The name of the user is ${user.name.first} ${user.name.last}`;
+        document.getElementById("email").innerHTML=`The Email of the user is ${user.email}`;
+        document.getElementById("photo").src=user.picture.large;
+        document.getElementById("location").innerHTML=`The location of the user is ${user.location.country}`;
+        document.getElementById("no").innerHTML=`The number of the user is ${user.phone}`;
+    }  
     catch(error){
         console.log(error);
+        console.log("There is a error");
     }
 
 }
