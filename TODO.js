88e3799785479f6fbce90c72f84a65c7cc9a190a
sanika14-sp <gsanikapm14@gmail.com>
2026-06-20 23:41:24 +0530

    display all feature added

--- TODO.js
+++ TODO.js
@@ -1,17 +1,4 @@
 let todo = JSON.parse(localStorage.getItem("todos")) || [];
-
-let displayTodo = () =>{
-     document.getElementById("display").innerHTML="";
-    let d = document.createElement("d");
-     displayTodo.appendchild(d);
-     todo.forEach((task)=>{
-      console.log(task);
-
-    });
-
-}
-displayTodo();
-
 let Create_todo= ()=>{
     let todoInput = document.getElementById("input_box");
     let task = todoInput.value;
@@ -20,8 +7,21 @@ let Create_todo= ()=>{
     todoInput.value="";
 }
 
-
 let Delete_todo= ()=>{
     
 }
 
+let displayTodo = () =>{
+     let displayDiv= document.getElementById("display")
+     displayDiv.innerHTML= "";
+
+     todo.forEach((task)=>{
+        console.log(task);
+        let p = document.createElement("p");
+        p.innerText= task;
+        displayDiv.appendChild(p);
+
+     })
+
+}
+displayTodo();
